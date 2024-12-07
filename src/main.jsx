a5d599b40da4bf40dbdffd59a2a07090d33ef066
gr99murad <golamrabbanimurad@gmail.com>
2024-12-07 11:54:03 +0600

    Added json data20 countries

--- src/main.jsx
+++ src/main.jsx
@@ -1,17 +1,17 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.jsx'
 
 import {
   createBrowserRouter,
   RouterProvider,
 } from "react-router-dom";
+import Home from './Layouts/Home.jsx';
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <App></App>,
+    element:<Home></Home>,
   },
   
 ]);
